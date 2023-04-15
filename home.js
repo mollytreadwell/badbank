97955a22248b12d1d1e99f6979cbfec83222040f
Molly Treadwell <106624903+mollytreadwell@users.noreply.github.com>
2023-04-15 13:16:08 -0400

    Update home.js

--- home.js
+++ home.js
@@ -8,9 +8,9 @@ function Home(){
             text="Baddest Bank Around"
             body={(
             <>
-            <img src="/pigbank.jpg" className="img-fluid" alt="Responsive image" width="350"/><br/>
-            <Link to="./createaccount"><button type="submit" className="btn btn-dark">Create Account</button></Link>
-            <Link to="./login"><button type="submit" className="btn btn-dark">Login</button></Link>
+            <img src="pigbank.jpg" className="img-fluid" alt="Responsive image" width="350"/><br/>
+            <Link to="/createaccount"><button type="submit" className="btn btn-dark">Create Account</button></Link>
+            <Link to="/login"><button type="submit" className="btn btn-dark">Login</button></Link>
             </>
             )}
         />
